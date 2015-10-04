3dfd2642d0e0bce570e0197d5e28bce62f63d4a3
HCastano <castano.ha@gmail.com>
2015-10-04 14:58:43 -0400

    Finish JS translation + sketchy fix for words that end in 'ee'

--- countsyl.js
+++ countsyl.js
@@ -1,93 +1,111 @@
-#!//usr/bin/env python
+//!//usr/bin/env python
 
-# Count syllables in a word.
-#
-# Doesn't use any fancy knowledge, just a few super simple rules:
-# a vowel starts each syllable;
-# a doubled vowel doesn't add an extra syllable;
-# two or more different vowels together are a diphthong,
-# and probably don't start a new syllable but might;
-# y is considered a vowel when it follows a consonant.
-#
-# Even with these simple rules, it gets results far better
-# than python-hyphenate with the libreoffice hyphenation dictionary.
-#
-# Copyright 2013 by Akkana Peck http://shallowsky.com.
-# Share and enjoy under the terms of the GPLv2 or later.
+// Count syllables in a word.
+// 
+// Doesn't use any fancy knowledge, just a few super simple rules:
+// a vowel starts each syllable;
+// a doubled vowel doesn't add an extra syllable;
+// two or more different vowels together are a diphthong,
+// and probably don't start a new syllable but might;
+// y is considered a vowel when it follows a consonant.
+// 
+// Even with these simple rules, it gets results far better
+// than python-hyphenate with the libreoffice hyphenation dictionary.
+// 
+// Copyright 2013 by Akkana Peck http://shallowsky.com.
+// Share and enjoy under the terms of the GPLv2 or later.
 
-import sys
 
-verbose = False
+var verbose = false;
 
-total = 0; 
+var total = 0; 
 
-def count_syllables(word):
-    vowels = ['a', 'e', 'i', 'o', 'u']
+var count_syllables = function(word){
+    var vowels = ['a', 'e', 'i', 'o', 'u'];
 
-    on_vowel = False
-    in_diphthong = False
-    minsyl = 0
-    maxsyl = 0
-    lastchar = None
-    numVowel = 0 
+    var on_vowel = false;
+    var in_diphthong = false;
+    var minsyl = 0;
+    var maxsyl = 0;
+    var lastchar = null;
+    var numVowel = 0 ;
 
-    word = word.lower()
-    for c in word:
-        is_vowel = c in vowels
+    word = word.toLowerCase();
+    for (var i in word){
+        var c = word[i];
+        var is_vowel = false;
+        for(var vowel in vowels){
+            is_vowel = is_vowel || vowels[vowel] == c;
+        }
 
-        if on_vowel == None:
-            on_vowel = is_vowel
+        if (on_vowel == null){
+            on_vowel = is_vowel;
+        }
 
-        # y is a special case
-        if c == 'y':
-            is_vowel = not on_vowel
+        // y is a special case
+        if (c == 'y'){
+            is_vowel = ! on_vowel;
+        }
 
-        if is_vowel:
-            numVowel+=1
-            if verbose: print (c), "is a vowel"
-            if not on_vowel:
-                # We weren't on a vowel before.
-                # Seeing a new vowel bumps the syllable count.
-                if verbose: print ("new syllable")
-                minsyl += 1
-                maxsyl += 1
-            elif on_vowel and not in_diphthong and c != lastchar:
-                # We were already in a vowel.
-                # Don't increment anything except the max count,
-                # and only do that once per diphthong.
-                if verbose: print (c), "is a diphthong"
-                in_diphthong = True
-                maxsyl += 1
-        elif verbose: print ("[consonant]")
+        if (is_vowel){
+            numVowel++;
+            if (verbose) {
+                console.log (c + " is a vowel");
+            }
+            if (!on_vowel){
+                // # We weren't on a vowel before.
+                // # Seeing a new vowel bumps the syllable count.
+                if (verbose){
+                    console.log("new syllable")
+                }
+                minsyl ++;
+                maxsyl ++;
+            }else if( on_vowel && ! in_diphthong && c != lastchar){
+                // # We were already in a vowel.
+                // # Don't increment anything except the max count,
+                // # and only do that once per diphthong.
+                if (verbose){ 
+                    console.log(c + " is a diphthong");
+                }
+                in_diphthong = true;
+                maxsyl ++;
+            }
+        }else if(verbose){
+            console.log ("[consonant]");
+        }
+        on_vowel = is_vowel;
+        lastchar = c;
+    }
 
-        on_vowel = is_vowel
-        lastchar = c
+    // # Some special cases:
+    if (word[word.length-1] == 'e'){
+        if (numVowel !=1){ //#account for short words that end with 'e'
+            if(numVowel == 2 && word[word.length-2] == 'e'){
+                minsyl ++;
+            }
+            minsyl --;
+        }
+    }
+    // # if it ended with a consonant followed by y, count that as a syllable.
+    if (word[word.length-1] == 'y' && ! on_vowel){
+        maxsyl ++;
+    }
 
-    # Some special cases:
-    if word[-1] == 'e':
-        if (numVowel !=1): #account for short words that end with 'e'
-            minsyl -= 1
-    # if it ended with a consonant followed by y, count that as a syllable.
-    if word[-1] == 'y' and not on_vowel:
-        maxsyl += 1
+    return minsyl;
+}
 
-    return minsyl, maxsyl
+var main = function(words){
+    for (var word in words){
+        word = word.trim();
+        smin = count_syllables(word);
+        total+= smax;
+        console.log (word, smax, smin) //#check, might be backwards
+    }
+    console.log (total);
 
-if __name__ == '__main__':
-    if sys.argv[1].startswith('-f'):
-        for word in open(sys.argv[2]):
-            word = word.strip()
-            smax, smin = count_syllables(word)
-            print (word, smax, smin)
-    else:
-        for word in sys.argv[1:]:
-            word = word.strip()
-            smax, smin = count_syllables(word)
-            total+= smax
-            print (word, smax, smin) #check, might be backwards
-    print (total)
-
-    if (total != 17):
-        print ("Not a haiku")
-    else:
-        print ("You made a goddam Haiku, gg")
+    if (total != 17){
+        console.log ("Not a haiku");
+    }else{
+        console.log ("You made a goddam Haiku, gg");    
+    }
+}
