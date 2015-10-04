e2ec8d740a77acf7e7de363af64d961768feba69
tueric <e2tu@uwaterloo.ca>
2015-10-04 15:38:29 -0400

    added landing page and a lot of stuffs

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 var socket = io();
 var type_audio = new Audio('resources/AIRHORN.mp3');
 var send_audio = new Audio('resources/triple.mp3')
+var username="";
+
 $(document).on("keyup", function() {
 	type_audio.pause();
 	type_audio.currentTime = 0;
@@ -8,17 +10,23 @@ $(document).on("keyup", function() {
 })
 
 var send = function() {
-			var message = $("#textbox").val();
-			message = message.replace(/\n/g, '<br>');
-			socket.emit("message", message);
-			$("#textbox").val("");
-			send_audio.pause();
-			send_audio.currentTime=0;
-			send_audio.play();
+	var message = $("#textbox").val();
+	message = message.replace(/\n/g, '<br>');
+	socket.emit("message", [message, username.toString()]);
+	$("#textbox").val("");
+	send_audio.pause();
+	send_audio.currentTime=0;
+	send_audio.play();
+};
+
+var disappear=function () {
+	$("#landing").slideToggle();
+	username=$("#landing input").val();
 };
 
 socket.on("message", function(message) {
-    $("#messages").append($("<li>").html(message));
-    var messages = document.getElementById("messages");
-    messages.scrollTop = messages.scrollHeight;
-})	
+	//console.log(message[1])
+	$("#messages").append($("<li>").html("<span style='color:red'>" + message[1]+"</span>" +  ":<br>" + message[0]));
+		var messages = document.getElementById("messages");
+		messages.scrollTop = messages.scrollHeight;
+	})	
